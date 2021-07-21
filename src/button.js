fd44fca3dc7b88dfcdf44098847605e6b58698a1
Jose Bolanos <bolanosjjr@gmail.com>
2021-07-20 21:38:30 -0500

    button start

---
+++ src/button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Button() {
+    return (
+        <Button variant="contained" color="primary">
+            Search
+        </Button>
+
+    )
+}
+
+export default Button;
