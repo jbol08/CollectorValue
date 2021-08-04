0c139770d27bd26d1371f7d23b8f4c2079e2b0af
Jose Bolanos <bolanosjjr@gmail.com>
2021-08-03 21:53:48 -0500

    navbar

---
+++ src/card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import api_key from './apiNum';
+
+const base_API = 'https://api.priceguide.cards/v1/embedded';
+
+function sportsCard(props) {
+    let img = `${base_API}/{props.term}`
+    return (
+        <div></div>
+    )
+}
+
+export default sportsCard;
--- src/nav.js
+++ src/nav.js
@@ -1,1 +1,25 @@
 import React from 'react';
+
+
+import { NavLink } from "react-router-dom";
+import { Navbar, Nav, NavItem } from "reactstrap";
+
+function NavBar() {
+  return (
+    <div>
+      <Navbar expand="md">
+        <NavLink exact to="/" className="navbar-brand">
+          Card Finder
+        </NavLink>
+
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink to="/">Home</NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+    </div>
+  );
+}
+
+export default NavBar;
